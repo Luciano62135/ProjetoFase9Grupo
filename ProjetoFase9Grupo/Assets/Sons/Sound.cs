bb67fa790d9d03face9cafa2221d10af60afa8a8
kikiArtur <artur.kniest@terra.com.br>
2021-10-22 14:54:11 -0300

    audio
    o audio tá funcionando

--- ProjetoFase9Grupo/Assets/Sons/Sound.cs
+++ ProjetoFase9Grupo/Assets/Sons/Sound.cs
@@ -12,4 +12,10 @@ public class Sound {
     public float volume;
     [Range(.1f, 3f)]
     public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+
 }
