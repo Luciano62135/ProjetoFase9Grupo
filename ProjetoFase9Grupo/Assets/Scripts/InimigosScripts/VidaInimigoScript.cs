c5b3a34fc7bf668ea2e0011ba150c8756c6b69a6
Luciano62135 <guimaraesluciano634@gmail.com>
2021-10-26 18:28:30 -0300

    Bonificação e timer
    sistema de bonificação do inimigo esta pronto e o timer também

--- ProjetoFase9Grupo/Assets/Scripts/InimigosScripts/VidaInimigoScript.cs
+++ ProjetoFase9Grupo/Assets/Scripts/InimigosScripts/VidaInimigoScript.cs
@@ -7,6 +7,9 @@ public class VidaInimigoScript : MonoBehaviour
     public float vidaAtual;
     public float vidaMaxima = 100;
     public BarraDeVida barraDeVida;
+    public GameObject moedaPrata;
+    public GameObject moedaCobre;
+    public int chanceDaMoeda;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +30,15 @@ public class VidaInimigoScript : MonoBehaviour
         barraDeVida.SetarVida(vidaAtual);
         if (vidaAtual <= 0f)
         {
+            chanceDaMoeda = Random.RandomRange(1, 2);
+            if (chanceDaMoeda == 1)
+            {
+                Instantiate(moedaCobre, transform.position, transform.rotation);
+            }
+            else if (chanceDaMoeda == 2)
+            {
+                Instantiate(moedaPrata, transform.position, transform.rotation);
+            }
             Destroy(this.gameObject);
         }
     }
