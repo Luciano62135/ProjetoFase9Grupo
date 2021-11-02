e6c95d2112f3fa0e9ab72503d82b05245717213f
Luciano62135 <guimaraesluciano634@gmail.com>
2021-11-02 19:27:09 -0300

    1

---
+++ ProjetoFase9Grupo/Assets/Scripts/ObjetosEItensScripts/Moedas/PocoesGeral.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PocoesGeral : MonoBehaviour
+{
+    private PlayerHud playerhud;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerhud = GameObject.Find("Canvas").GetComponent<PlayerHud>();
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKey(KeyCode.E))
+        {
+            playerhud.pocoes++;
+            Destroy(this.gameObject);
+        }
+    }
+}
