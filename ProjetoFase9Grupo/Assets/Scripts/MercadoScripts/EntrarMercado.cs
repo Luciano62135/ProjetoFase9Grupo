85f365a0cb545bc53f17efae6b1bacc9f4001c9d
Luciano62135 <guimaraesluciano634@gmail.com>
2021-11-25 22:30:34 -0300

    Mercado/Colisões
    Script para entrar no mercado e script de algumas colisões estão prontos.

---
+++ ProjetoFase9Grupo/Assets/Scripts/MercadoScripts/EntrarMercado.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class EntrarMercado : MonoBehaviour
+{
+    public GameObject mercado;
+    public GameObject entrarMercadoText;
+    public bool estaNoMercado;
+
+    PhotonView pv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (pv.IsMine)
+        {
+            if (other.tag == "Player")
+            {
+                entrarMercadoText.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E) && estaNoMercado == false)
+                {
+                    mercado.SetActive(true);
+                    estaNoMercado = true;
+                }
+                else if (Input.GetKeyDown(KeyCode.E) && estaNoMercado == true)
+                {
+                    mercado.SetActive(false);
+                    estaNoMercado = false;
+                }
+            }
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (pv.IsMine)
+        {
+            if (other.tag == "Player")
+            {
+                entrarMercadoText.SetActive(false);
+                mercado.SetActive(false);
+            }
+        }
+    }
+}
