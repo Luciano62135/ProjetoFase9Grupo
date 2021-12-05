1525fbaf14446d659e61acc9652e301dca940c9f
Luciano62135 <guimaraesluciano634@gmail.com>
2021-12-05 16:11:19 -0300

    1

--- ProjetoFase9Grupo/Assets/Scripts/MercadoScripts/Comprar.cs
+++ ProjetoFase9Grupo/Assets/Scripts/MercadoScripts/Comprar.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class Comprar : MonoBehaviour
 {
     private EntrarMercado player;
+    private PhotonView pv;
+
+    private float compras = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<EntrarMercado>();
+        pv = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
@@ -16,4 +22,24 @@ public class Comprar : MonoBehaviour
     {
         
     }
+
+    public void ComprarPocao()
+    {
+        if (pv.IsMine)
+        {
+
+        }
+    }
+
+    public void ComprarVidaMaxima()
+    {
+
+    }
+
+    public void ComprarMelhoriaArma()
+    {
+
+    }
+
+
 }
